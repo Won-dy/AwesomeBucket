8268aa72543bb42a99c73de576b92b3b3133d23f
Won-dy <junhfireace@gmail.com>
2022-05-18 17:05:53 +0900

    Refactor: Spring Data JPA 기반 UserRepository
    순수 JPA 기반 -> Spring Data JPA 기반 Repository로 리팩토링

--- Backend/src/main/java/dyveloper/awesomebucket/domain/user/UserRepository.java
+++ Backend/src/main/java/dyveloper/awesomebucket/domain/user/UserRepository.java
@@ -1,22 +1,9 @@
 package dyveloper.awesomebucket.domain.user;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.persistence.EntityManager;
 import java.util.Optional;
 
-@Repository
-@RequiredArgsConstructor
-public class UserRepository {
-
-    private final EntityManager em;
-
-    // 이메일로 회원 조회
-    public Optional<User> findByEmail(String email) {
-        return em.createQuery("select u from User u where u.email = :email", User.class)
-                .setParameter("email", email).getResultList()
-                .stream().findFirst();
-    }
-
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
 }
