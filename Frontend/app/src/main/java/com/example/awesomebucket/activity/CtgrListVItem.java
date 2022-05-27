5a72d67ffaad6d2d4866114a6ed2ec45224fcf43
Won-dy <junhfireace@gmail.com>
2022-05-27 12:09:19 +0900

    Fix: 변수명으로 인한 Getter 인식 오류 수정

--- Frontend/app/src/main/java/com/example/awesomebucket/activity/CtgrListVItem.java
+++ Frontend/app/src/main/java/com/example/awesomebucket/activity/CtgrListVItem.java
@@ -12,5 +12,5 @@ public class CtgrListVItem {  // 각 아이템에 출력될 데이터를 위한 
     // 클래스 멤버 변수 정의
     private long id;  // 카테고리 ID
     private boolean isDefault;  // 디폴트 여부
-    private String nameStr;
+    private String name;
 }
