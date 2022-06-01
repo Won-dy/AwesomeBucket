1edcbc8636d23ba47a6890591705459676f12106
Won-dy <junhfireace@gmail.com>
2022-06-01 14:12:09 +0900

    Fix: 이메일 유효성 검사 정규표현식 변경

--- Frontend/app/src/main/java/com/example/awesomebucket/MyConstant.java
+++ Frontend/app/src/main/java/com/example/awesomebucket/MyConstant.java
@@ -7,7 +7,7 @@ public class MyConstant {
     public static final long NO_ID = -2147483648;  // 값이 없을 때 설정할 기본 ID 값
 
     // 정규표현식
-    public static final String emailPattern = "^[a-zA-Z0-9]+@[a-zA-Z0-9]+$";  // 이메일 패턴 정규식
+    public static final String emailPattern = "^(.+)@(.+)$";  // 이메일 패턴 정규식
     public static final String pwPattern = "^((?=.*\\d)(?=.*[a-zA-Z]).{8,20})$";  // 비밀번호 패턴(영문, 숫자를 포함한 8~20자) 정규식
     public static final String blankPattern = "(\\s)";  // 공백 문자 정규식
 
